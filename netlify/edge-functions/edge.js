261084657c5f50dad8996e64a231e614010ec1b6
Bryce Wray <bwray@wraytx.com>
2023-03-12 17:22:45 -0500

    Another try at headers (resp this time)

--- netlify/edge-functions/edge.js
+++ netlify/edge-functions/edge.js
@@ -1,3 +1,13 @@
 export default async (request, context) => {
-  request.headers.set("X-BW-Custom-Header", "Your custom header value");
+  const url = new URL(request.url);
+
+  if (url.searchParams.get("method") !== "set-response-header") {
+    return context.next();
+  }
+
+  console.log(`Adding a custom header to the response for ${url}`);
+
+  const response = await context.next();
+  response.headers.set("X-Your-Custom-Header", "Your custom header value");
+  return response;
 };
