4ec7c48e9eca0f6d60e6e34d75d6454ead156c5e
Bryce Wray <bwray@wraytx.com>
2023-03-17 11:25:41 -0500

    Further

--- netlify/functions/caches-nonces.js
+++ netlify/functions/caches-nonces.js
@@ -1,15 +1,3 @@
 export default async (request, context) => {
-  const url = new URL(request.url)
-
-  if (url.searchParams.get("method") !== "set-response-header") {
-    return context.next()
-  }
-
-	let valForHeader = 3138000
-
-  console.log(`Adding a custom header to the response for ${url}`)
-
-  const response = await context.next()
-  response.headers.set("X-BW-test-header", valForHeader)
-  return response
+  request.headers.set("X-BW-test-header", valForHeader)
 }
