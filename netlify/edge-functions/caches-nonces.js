fdb4b972e1b2828b09ef73631b30b584f156cf52
Bryce Wray <bwray@wraytx.com>
2023-03-17 11:41:32 -0500

    Try with nonce-gen

--- netlify/edge-functions/caches-nonces.js
+++ netlify/edge-functions/caches-nonces.js
@@ -1,6 +1,20 @@
+function dec2hex(dec) {
+  return ("0" + dec.toString(16)).substr(-2)
+}
+
+function generateNonce() {
+  const arr = new Uint8Array(12)
+  crypto.getRandomValues(arr)
+  const values = Array.from(arr, dec2hex)
+  return [
+    btoa(values.slice(0, 5).join("")).substr(0, 14),
+    btoa(values.slice(5).join("")),
+  ].join("/")
+}
+
 export default async (request, context) => {
-	let valForHeader = 3138000
+	const nonce = generateNonce()
   const response = await context.next()
-  response.headers.set("X-BW-Test-Header", valForHeader)
+  response.headers.set("X-BW-Test-Header", nonce)
   return response
 }
